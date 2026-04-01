afcd037e1d55c847bc208026e65865c2ff4c9cdc
cataliiin <cataliiin9@gmail.com>
2026-04-01 23:44:10 +0300

    Refactor cube to support textures

--- Engine/Entities/3D/Cube3D.cpp
+++ Engine/Entities/3D/Cube3D.cpp
@@ -8,6 +8,72 @@ Cube3D::Cube3D() : Entity3D() {
     transform.scale = Vec3(1.0f, 1.0f, 1.0f);
 }
 
-void Cube3D::Draw() {
-    glutSolidCube(1.0); 
+void Cube3D::Draw()
+{
+    float s = 0.5f;
+
+    glBegin(GL_QUADS);
+
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-s, -s, s);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(s, -s, s);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(s, s, s);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-s, s, s);
+
+    // Fața DIN SPATE (Z-)
+    glNormal3f(0.0f, 0.0f, -1.0f);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-s, -s, -s);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-s, s, -s);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(s, s, -s);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(s, -s, -s);
+
+    glNormal3f(0.0f, 1.0f, 0.0f);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-s, s, -s);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-s, s, s);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(s, s, s);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(s, s, -s);
+
+    glNormal3f(0.0f, -1.0f, 0.0f);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-s, -s, -s);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(s, -s, -s);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(s, -s, s);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-s, -s, s);
+
+    glNormal3f(1.0f, 0.0f, 0.0f);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(s, -s, -s);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(s, s, -s);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(s, s, s);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(s, -s, s);
+
+    glNormal3f(-1.0f, 0.0f, 0.0f);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-s, -s, -s);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-s, -s, s);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-s, s, s);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-s, s, -s);
+
+    glEnd();
 }
